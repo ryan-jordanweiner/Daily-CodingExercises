5cf864036e4ae916021648ddda8b3b910b66ef50
Ryan Weiner <79421575+ryan-jordanweiner@users.noreply.github.com>
2021-02-21 15:47:11 -0500

    Update BubbleSort.java

--- Sorting/BubbleSort/BubbleSort.java
+++ Sorting/BubbleSort/BubbleSort.java
@@ -1,33 +1,16 @@
 import java.util.Arrays;
-public class BubbleSort
-{
-    public int[] bubbleSort(int[] ogArr)
-    {
-        while (!(isSorted(ogArr)))
-        {
-            for (int i = 1; i < ogArr.length; i++)
-            {
-                if (ogArr[i-1] > ogArr[i])
-                {
-                    int tempMin, tempMax;
-                    tempMax = ogArr[i-1];
-                    tempMin = ogArr[i];
-
-                    ogArr[i-1] = tempMin;
-                    ogArr[i] = tempMax;
-
-                }
-            }
-            System.out.println(Arrays.toString(ogArr));
-        }import java.util.Arrays;
 public class BubbleSortSolution
 {
     public int[] bubbleSort(int[] ogArr)
     {
+        
+        //while the array is not in order
         while (!(isSorted(ogArr)))
         {
+            iterate over the array
             for (int i = 1; i < ogArr.length; i++)
             {
+                //compare the values of 2 adjacent elements; if the first element is larger, swap the elements 
                 if (ogArr[i-1] > ogArr[i])
                 {
                     int tempMin, tempMax;
@@ -39,32 +22,18 @@ public class BubbleSortSolution
 
                 }
             }
-            System.out.println(Arrays.toString(ogArr));
         }
+        //return the sorted array
         return ogArr;
     }
-
+    
+    //method to check if the array is sorted
     public boolean isSorted(int[] arr)
     {
+        //iterate over the array
         for(int i = 1; i < arr.length; i++)
         {
-            if (arr[i-1] > arr[i])
-            {
-                return false;
-            }
-
-        }
-        return true;
-    }
-}
-
-        return ogArr;
-    }
-
-    public boolean isSorted(int[] arr)
-    {
-        for(int i = 1; i < arr.length; i++)
-        {
+            //compare adjacent values; if the first one is larger, return false
             if (arr[i-1] > arr[i])
             {
                 return false;
