b35023fb5a549ab682998cb4921be6763c510aae
Ryan Weiner <79421575+ryan-jordanweiner@users.noreply.github.com>
2021-02-26 14:44:13 -0500

    Create getIndex.cpp

---
+++ Searches/GetIndex/getIndex.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+int getIndex(int[], int);
+
+int main()
+{
+    using namespace std;
+
+    getIndex(arr, element)
+
+    return 0;
+}
+
+
+ing getIndex(int[] arr, element)
+{
+
+    for (int i = 0; i<sizeof(arr), i++)
+    {
+        if (arr[i]==element)
+        {
+            return i;
+        }
+    }
+
+
+    return null;
+}
